26491905437be147f32b0d4cdcd2c7d48ed3ae22
Shmiddy <eric.rackear@gmail.com>
2014-04-03 22:32:59 -0400

    Added Right Bounds

---
+++ OriginalOrigins.git/branches/Assets/Scripts/NextLevelScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevelScript : MonoBehaviour
+{
+
+    public Collider2D collider;
+    public string level;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        Application.LoadLevel(level);
+    }
+
+}
