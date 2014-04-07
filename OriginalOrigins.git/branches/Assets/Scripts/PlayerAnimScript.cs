58a5cd73a64b4af0c289d00827a394cb4f729f63
Shmiddy <eric.rackear@gmail.com>
2014-04-07 06:55:51 -0400

    Added Animator, Added Animation script, added left facing sprites, added left and right bobbing animations
    Changed character to playerV2 on all levels, this new character uses animation and physics and all buttons were changed accordingly
    
    Updated speeds to 10 on all levels except for last level and falling level

---
+++ OriginalOrigins.git/branches/Assets/Scripts/PlayerAnimScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimScript : MonoBehaviour {
+
+    private Animator animator;
+    public GUITexture rightButton;
+    public GUITexture leftButton;
+
+	// Use this for initialization
+	void Start () {
+
+        animator = this.GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    foreach(Touch touch in Input.touches)
+        {
+            //for right button press
+            if(rightButton.HitTest(touch.position) && touch.phase != TouchPhase.Ended)
+            {
+                animator.SetInteger("Direction", 1);
+                animator.SetFloat("Speed", 1.0f);
+            }
+
+            //for left button press
+            else if (leftButton.HitTest(touch.position) && touch.phase != TouchPhase.Ended)
+            {
+                animator.SetInteger("Direction", 0);
+                animator.SetFloat("Speed", 1.0f);
+            }
+
+            //for no button press
+            else
+            {
+                animator.SetFloat("Speed", 0.0f);
+            }
+        }
+	}
+}
