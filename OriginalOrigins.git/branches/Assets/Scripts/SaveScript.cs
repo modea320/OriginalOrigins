18ab15e95b22031e74a2642fedc12ffe42329129
Shmiddy <eric.rackear@gmail.com>
2014-04-09 20:29:34 -0400

    Added pause menu and saving function. Pause menu is currently only on level 1, still needs visual tweaking. Save function works on all levels, even cutscenes. Upon restarting the app, the initial menu screen appears and then the last level is instantly loaded.

---
+++ OriginalOrigins.git/branches/Assets/Scripts/SaveScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveScript : MonoBehaviour {
+
+    int level;
+    int isLoaded;
+
+	// Use this for initialization
+	void Start () {
+
+        level = PlayerPrefs.GetInt("LastLevel");
+        isLoaded = PlayerPrefs.GetInt("Loaded");
+
+        if(isLoaded == 1)
+        {
+            Application.LoadLevel(level);
+            PlayerPrefs.SetInt("Loaded", 0);
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //string currentLevel = Application.loadedLevel;
+        //Debug.Log(Application.loadedLevel);
+        //Save(currentLevel);
+
+	}
+
+    void OnApplicationQuit()
+    {
+        //Save(Application.loadedLevel);
+        PlayerPrefs.SetInt("LastLevel", Application.loadedLevel);
+
+        //set to 1 if a scene is saved, then set this to zero upon resuming the application
+        PlayerPrefs.SetInt("Loaded", 1);
+    }
+
+    void OnApplicationPause()
+    {
+        PlayerPrefs.SetInt("LastLevel", Application.loadedLevel);
+
+        //set to 1 if a scene is saved, then set this to zero upon resuming the application
+        PlayerPrefs.SetInt("Loaded", 1);
+
+    }
+
+    void Save(int level)
+    {
+        PlayerPrefs.SetInt("LastLevel", level);
+    }
+
+    
+}
