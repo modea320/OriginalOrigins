96f7c1b4cd71e8b59f829f351462d7a30d2417fe
Shmiddy <eric.rackear@gmail.com>
2014-04-12 13:32:31 -0400

    Added Aah, Aah's animation, and the following script (although it needs work to "snake" better)

---
+++ OriginalOrigins.git/branches/Assets/Scripts/AahAnimScript.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AahAnimScript : MonoBehaviour {
+
+
+    public GameObject master;
+    public GameObject follower;
+    Transform leader;
+    public int delay;
+    //public float delay = 0.06f;
+    private Animator animator;
+
+    Vector3 offsetPos;
+    Vector3 currentPos;
+    Vector3 targetPos;
+
+    Vector3[] posArray = new Vector3[10];
+    int count;
+
+    //Quaternion startRot;
+    //Quaternion leaderRot;
+
+
+	// Use this for initialization
+	void Start () {
+
+        
+        leader = master.transform;
+
+        posArray[0] = leader.position;
+
+        //offsetPos = follower.transform.position - leader.position;
+
+        animator = this.GetComponent<Animator>();
+
+        //startRot = transform.rotation;
+        //leaderRot = leader.rotation;
+
+	}
+
+    /*
+    void LateUpdate()
+    {
+
+        
+        var targetPos = offsetPos + leader.position;
+
+        //var targetRot = Quaternion.Inverse(leaderRot) * leader.rotation * startRot;
+
+        var dT = Time.deltaTime / delay;
+        follower.transform.position = Vector3.Lerp(transform.position, targetPos, dT);
+
+        //follower.transform.position = transform.position;
+        //transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, dT);
+
+    }
+	*/
+
+	// Update is called once per frame
+	void Update () {
+        if (count < 10)
+        {
+            posArray[count] = leader.position;
+            count++;
+        }
+        else
+        {
+            count = 0;
+            posArray[count] = leader.position;
+            count++;
+        }
+
+        if(count-delay < 0)
+        {
+            follower.transform.position = posArray[posArray.Length - delay];
+
+        }
+
+        else
+        {
+            follower.transform.position = posArray[count - delay];
+        }
+        
+
+
+        animator.SetFloat("Speed", 0.0f);
+
+	}
+}
