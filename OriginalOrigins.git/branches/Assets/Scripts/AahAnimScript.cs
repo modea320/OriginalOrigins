305fc345e6c6f5013932ab70d565b6b00e47051b
Shmiddy <eric.rackear@gmail.com>
2014-04-13 00:31:41 -0400

    Improved Aah's animation/following script, although I still need to make it so Aah follows Ooh when he jumps Changed Ooh in all levels after level 1 to darker sprite and animation

--- OriginalOrigins.git/branches/Assets/Scripts/AahAnimScript.cs
+++ OriginalOrigins.git/branches/Assets/Scripts/AahAnimScript.cs
@@ -7,7 +7,11 @@ public class AahAnimScript : MonoBehaviour {
     public GameObject master;
     public GameObject follower;
     Transform leader;
-    public int delay;
+    public float delay;
+    public float threshold;
+    
+
+
     //public float delay = 0.06f;
     private Animator animator;
 
@@ -15,11 +19,13 @@ public class AahAnimScript : MonoBehaviour {
     Vector3 currentPos;
     Vector3 targetPos;
 
-    Vector3[] posArray = new Vector3[10];
-    int count;
+    Vector3[] posArray = new Vector3[2];
+    int count = 0;
+    bool isMoving;
+    bool movingLeft;
+    bool movingRight;
 
-    //Quaternion startRot;
-    //Quaternion leaderRot;
+    
 
 
 	// Use this for initialization
@@ -28,37 +34,139 @@ public class AahAnimScript : MonoBehaviour {
         
         leader = master.transform;
 
-        posArray[0] = leader.position;
+        posArray[0] = follower.transform.position;
 
-        //offsetPos = follower.transform.position - leader.position;
+        offsetPos = follower.transform.position - leader.position;
 
         animator = this.GetComponent<Animator>();
 
-        //startRot = transform.rotation;
-        //leaderRot = leader.rotation;
+      
 
 	}
 
-    /*
+    
     void LateUpdate()
     {
+        // toggle between 0 and 1 for each frame
+        if(count == 0)
+        {
+            count = 1;
+        }
+        else
+        {
+            count = 0;
+        }
 
+        // add current position to array
+        posArray[count] = follower.transform.position;
+
+        // check posArray[1] - posArray[0]
+        if (count == 0)
+        {
+            // if the current and previous position are equal, the object isn't moving
+            if (posArray[1] - posArray[0] == new Vector3(0, 0, 0) || Mathf.Abs(posArray[1].x - posArray[0].x) < threshold)
+            {
+                isMoving = false;
+            }
+            else
+            {
+                isMoving = true;
+
+                // if current minus past position is negative, is moving to the left
+                if(posArray[1].x - posArray[0].x < 0)
+                {
+                    movingLeft = true;
+                    movingRight = false;
+
+                }
+
+                // otherwise moving to the right
+                else
+                {
+                    movingLeft = false;
+                    movingRight = true;
+                }
+                
+            }
+        }
         
+        // check posArray[0] - posArray[1]
+        else
+        {
+            if (posArray[1] - posArray[0] == new Vector3(0, 0, 0) || Mathf.Abs(posArray[1].x - posArray[0].x) < threshold)
+            {
+                isMoving = false;
+            }
+            else
+            {
+                isMoving = true;
+
+                // if current minus past position is positive, is moving to the left
+                // since now the positions are swapped, so the subtractions directions are
+                // opposite.. sorry that this is confusing
+                if(posArray[1].x - posArray[0].x > 0)
+                {
+                    movingLeft = true;
+                    movingRight = false;
+
+                }
+
+                // otherwise moving to the right
+                else
+                {
+                    movingLeft = false;
+                    movingRight = true;
+                }
+            }
+        }
+
+
         var targetPos = offsetPos + leader.position;
 
-        //var targetRot = Quaternion.Inverse(leaderRot) * leader.rotation * startRot;
+        float dT = Time.deltaTime / delay;
 
-        var dT = Time.deltaTime / delay;
+        float updatePos = Mathf.Abs(follower.transform.position.x - leader.position.x);
+
+        if(updatePos > offsetPos.x * 1.5)
+        {
+            dT *= 1.0f;
+        }
+
+        
+
         follower.transform.position = Vector3.Lerp(transform.position, targetPos, dT);
 
-        //follower.transform.position = transform.position;
-        //transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, dT);
+        
+
+        if(isMoving == false)
+        {
+            animator.SetFloat("Speed", 0.0f);
+        }
+        // moving to the left, left animation
+        else if(movingLeft)
+        {
+            animator.SetInteger("Direction", 0);
+            animator.SetFloat("Speed", 1.0f);
+        }
+        
+        // moving to the right, right animation
+        else if (movingRight)
+        {
+            animator.SetInteger("Direction", 1);
+            animator.SetFloat("Speed", 1.0f);
+        }
+
+        // if not moving, no animation
+        
+       
 
     }
-	*/
+	
 
 	// Update is called once per frame
 	void Update () {
+        /*
+        // block that updates the array storing the positions of the master
         if (count < 10)
         {
             posArray[count] = leader.position;
@@ -73,7 +181,7 @@ public class AahAnimScript : MonoBehaviour {
 
         if(count-delay < 0)
         {
-            follower.transform.position = posArray[posArray.Length - delay];
+            follower.transform.position = posArray[posArray.Length - delay] - gap;
 
         }
 
@@ -85,6 +193,6 @@ public class AahAnimScript : MonoBehaviour {
 
 
         animator.SetFloat("Speed", 0.0f);
-
+        */
 	}
 }
