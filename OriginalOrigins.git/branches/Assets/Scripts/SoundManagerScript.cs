0fe25f9c0d186a133d5485549ecc974236cd7c58
Shmiddy <eric.rackear@gmail.com>
2014-04-14 03:21:00 -0400

    Fixed audio added new audio manager script

---
+++ OriginalOrigins.git/branches/Assets/Scripts/SoundManagerScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManagerScript : MonoBehaviour {
+
+    public static SoundManagerScript Instance;
+
+    public AudioClip jump_sound;
+    public AudioClip lose_sound;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("Multiple Instances of SoundManagerScript");
+        }
+        Instance = this;
+    }
+
+    public void MakeLoseSound()
+    {
+        MakeSound(lose_sound);
+    }
+
+    public void MakeJumpSound()
+    {
+        MakeSound(jump_sound);
+    }
+
+    public void MakeSound(AudioClip theClip)
+    {
+        AudioSource.PlayClipAtPoint(theClip, transform.position);
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
