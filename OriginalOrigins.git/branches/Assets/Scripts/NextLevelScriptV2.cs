5cd81de3352d33f014399d92154067e117d780e9
Shmiddy <eric.rackear@gmail.com>
2014-04-18 06:36:35 -0400

    All cutscenes now working added two new scripts that load cutscenes via an integer instead of a string, that seemed to be the problem with the new cutscenes
    re-enabled the next level buttons for debugging purposes, but I played though all the levels that are beatable and watched all cutscenes to make sure that they work, and they do

---
+++ OriginalOrigins.git/branches/Assets/Scripts/NextLevelScriptV2.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevelScriptV2 : MonoBehaviour {
+
+    public int level;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // loads the level indicated by the public string level
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        Application.LoadLevel(level);
+    }
+}
